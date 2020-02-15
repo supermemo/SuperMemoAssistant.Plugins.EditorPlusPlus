fa55a48251ac752b8b7a868feac350b1775e4460
james <james@DESKTOP-JDJA6NK>
2020-02-15 14:03:57 +0000

    add a plain text paste to paste menu

---
+++ EditorPlusPlusPlugin.cs
@@ -0,0 +1,99 @@
+﻿#region License & Metadata
+
+// The MIT License (MIT)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// 
+// 
+// Created On:   2019/04/22 17:20
+// Modified On:  2019/04/22 20:52
+// Modified By:  Alexis
+
+#endregion
+
+
+using System.Windows.Input;
+using SuperMemoAssistant.Services;
+using SuperMemoAssistant.Services.IO.Keyboard;
+using SuperMemoAssistant.Services.Sentry;
+using SuperMemoAssistant.Sys.IO.Devices;
+using System.Windows;
+using SuperMemoAssistant.Extensions;
+using SuperMemoAssistant.Plugins.EditorPlusPlus.UI;
+
+namespace SuperMemoAssistant.Plugins.EditorPlusPlus
+{
+  // ReSharper disable once UnusedMember.Global
+  // ReSharper disable once ClassNeverInstantiated.Global
+  public class EditorPlusPlus : SentrySMAPluginBase<EditorPlusPlus>
+  {
+    #region Constructors
+
+    public EditorPlusPlus() { }
+
+    #endregion
+
+
+
+
+    #region Properties Impl - Public
+
+    /// <inheritdoc />
+    public override string Name => "EditorPlusPlus";
+
+    public override bool HasSettings => false;
+
+    #endregion
+
+
+    #region Methods Impl
+
+    /// <inheritdoc />
+    protected override void PluginInit()
+    {
+      Svc.HotKeyManager.RegisterGlobal(
+        "AdvancedPasteWindow",
+        "Open AdvancedPasteWindow",
+        HotKeyScope.SM,
+        new HotKey(Key.B, KeyModifiers.CtrlShift),
+        OpenAdvancedPasteMenu
+      );
+    }
+
+    public void OpenAdvancedPasteMenu()
+    {
+
+      Application.Current.Dispatcher.Invoke(
+        () =>
+        {
+          var wdw = new AdvancedPasteMenu();
+          wdw.ShowAndActivate();
+        }
+      );
+    }
+
+    
+    /// <inheritdoc />
+    public override void ShowSettings()
+    {
+    }
+
+    #endregion
+  }
+}
---
+++ UI/AdvancedPasteMenu.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HtmlAgilityPack;
+using SuperMemoAssistant.Extensions;
+using SuperMemoAssistant.Interop.SuperMemo.Content.Controls;
+using SuperMemoAssistant.Services;
+using mshtml;
+
+namespace SuperMemoAssistant.Plugins.EditorPlusPlus.UI
+{
+  /// <summary>
+  /// Interaction logic for AdvancedPasteMenu.xaml
+  /// </summary>
+  public partial class AdvancedPasteMenu
+  {
+
+    public AdvancedPasteMenu()
+    {
+      InitializeComponent();
+      
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+      switch (e.Key)
+      {
+        case Key.Enter:
+          OkBtn_Click();
+          break;
+        case Key.Escape:
+          Close();
+          break;
+      }
+    }
+
+    private void ExecuteSelected()
+    {
+      if (PlainTextPaste.IsChecked == true)
+      {
+        string clipboardText = GetClipboardText();
+        if (!string.IsNullOrEmpty(clipboardText))
+        {
+          string cleanText = FilterHtmlTags(clipboardText);
+          if (!string.IsNullOrEmpty(cleanText))
+          {
+            PasteText(cleanText);
+          }
+        }
+        
+      }
+      Close();
+    }
+
+    private string GetClipboardText()
+    {
+      if (Clipboard.ContainsText())
+      {
+        return Clipboard.GetText();
+      }
+
+      return null;
+    }
+
+    private string FilterHtmlTags(string text)
+    {
+      if (string.IsNullOrEmpty(text))
+      {
+        return text;
+      }
+      var doc = new HtmlDocument();
+      doc.LoadHtml(text);
+      return doc.DocumentNode.InnerText;
+    }
+
+    private void PasteText(string text)
+    {
+        IControlHtml ctrlHtml = Svc.SM.UI.ElementWdw.ControlGroup.FocusedControl.AsHtml();
+
+        if (ctrlHtml != null)
+        {
+          var htmlDoc = ctrlHtml.GetDocument();
+          var htmlSelObj = htmlDoc?.selection;
+
+          if (htmlSelObj?.createRange() is IHTMLTxtRange textSel)
+            textSel.text = text;
+        }
+    }
+
+    private void OkBtn_Click(object sender, RoutedEventArgs e)
+    {
+      ExecuteSelected();
+    }
+  }
+}
